0d233b2241d30848a2383c8f5e32665edcc7c2d1
pedro_espigares <pedritoesp04@gmail.com>
2023-01-12 09:24:18 +0100

    V2

--- ajax.js
+++ ajax.js
@@ -1,5 +1,7 @@
 const { createApp } = Vue
 
+
+
   createApp({
     data() {
       return {
@@ -7,61 +9,102 @@ const { createApp } = Vue
         tracks: undefined,
         client_id: "3a05bf07db6844a5a7e244e5cd6ca6a0",
         client_secret: "34eee371f8f74fc3ae4dfb6c34787e21",
-        access_token: "BQDzoqCqBL2K9PD5eBMn9Obw22kMB6u1StsSGQeMX1KbREnL0oU1ShD1iiRQmOr1Q79GCGmhUj79vtMaBj0-KiQC0DrXYc5pqjID84y8o17kB1kj2cA",
-        authParameters: {
-            method:"POST",
-            headers: {
-                "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: "grant_type=client_credentials&client_id=" + this.client_id + "&client_secret=" + this.client_secret
-        },
+        access_token: "",
         offset: 0,
-        request: undefined,
-        configs:{
+
+    // Configuración para las peticiones a la API de Spotify
+        headers:{
             Accept:"application/json",
            "Content-Type":"application/json",
            Authorization:"Bearer " + this.access_token
-        }
+        },
       }
     },
     methods: {
-        throwPetition(firstTime){
+        throwPetition(scroll = false){
             var throwed = false;
-            if(firstTime && !throwed){
+            if(!throwed){
+                this.offset = 0;
                 throwed = true;
 
-                axios.get('https://api.spotify.com/v1/search?q=' + this.busqueda + '&type=track&offset=' + this.offset, this.configs)
-                    .then((data) => {
-                        this.tracks = data.data.tracks.items;
-                    })
-                    .catch(error => console.log(error));
+                axios({
+                    method: 'get',
+                    url: 'https://api.spotify.com/v1/search?q=' + this.busqueda + '&type=track&offset=' + this.offset,
+                    headers: this.headers
+                }).then((data) => {
+                    this.tracks = data.data.tracks.items;
+                })
+                .catch(error => console.log(error));
+                
                 throwed = false;
                 this.offset += 20;
-            } else {
-                throwed = true;
-                if (this.busqueda != ""){
-                    axios.get('https://api.spotify.com/v1/search?q=' + this.busqueda + '&type=track&offset=' + this.offset, this.configs)
-                        .then((data) => {
-                            this.tracks = data.data.tracks.items;
-                        })
-                        .catch(error => console.log(error));
-                }
+            }
 
-                throwed = false;
+            if(scroll){
+                axios({
+                    method: 'get',
+                    url: 'https://api.spotify.com/v1/search?q=' + this.busqueda + '&type=track&offset=' + this.offset,
+                    headers: this.headers
+                }).then((data) => {
+                    this.tracks = this.tracks.concat(data.data.tracks.items);
+                })
+                .catch(error => console.log(error));
+
                 this.offset += 20;
             }
         },
 
         clearResults(){
-            document.getElementById("results").innerHTML = "";
+            this.offset = 0;
+            this.tracks = undefined;
             this.busqueda = "";
+        },
+
+        audioHandler(e){
+            var audio = e.target.parentElement.parentElement.getElementsByTagName("audio")[0];
+            if(e.target.classList.contains("fa-play")){
+                audio.volume = 0.2;
+                audio.play();
+
+                e.target.classList.remove("fa-play");
+                e.target.classList.add("fa-pause");
+
+                if(audio.ended){
+                    e.target.classList.remove("fa-play");
+                    e.target.classList.add("fa-pause");
+                }
+            } else if (e.target.classList.contains("fa-pause")){
+                audio.pause();
+                
+                e.target.classList.remove("fa-pause");
+                e.target.classList.add("fa-play");
+            }
         }
     },
     mounted(){
+        axios({
+
+            // Esta sintaxis es para la peticion POST del access_token de la API
+            method: 'post',
+            url: 'https://accounts.spotify.com/api/token',
+            data: "grant_type=client_credentials",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            auth: {
+                username: this.client_id,
+                password: this.client_secret
+            }
+            }).then((data) => {
+                this.access_token = data.data.access_token;
+                this.headers.Authorization = "Bearer " + this.access_token;
+            })
+
         window.addEventListener('scroll',()=>{
             if(window.scrollY + window.innerHeight >= 
             document.documentElement.scrollHeight){
-                this.throwPetition(false);
+                this.throwPetition(true);
             }
         })
     },
